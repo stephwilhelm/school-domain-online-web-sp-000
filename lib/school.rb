b422cbcf97a225a9aca96b99129baf9bebc8a784
Steph W <wilhelm.steph@gmail.com>
2020-05-04 23:55:44 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
-# code here!
+class School
+  attr_accessor :roster, :student_name, :school_name
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = Hash.new{ |h, k| h[k] = [] }
+  end
+
+  def add_student(student_name, grade)
+    if @roster[grade]
+       @roster[grade] << student_name
+    else
+       @roster[grade] = []
+       @roster[grade] << student_name
+    end
+  end
+
+  def grade
+
+  end
+
+  def sort(name)
+    
+  end
+end
